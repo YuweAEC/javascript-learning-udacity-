61f04e61cb707aaf7401bebd5e5713818afbb727
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-01 10:05:45 +0530

    adding new txt on variableswq

---
+++ keywords/variables.js
@@ -0,0 +1,3 @@
+//to create a variable in javascript
+
+var variablename = value; //here use var keyword before assigning any variable and then type the varable name you want to assign and then put assignment operator, i.e, the equals to sign and then put the value to want to put inside the variable.
