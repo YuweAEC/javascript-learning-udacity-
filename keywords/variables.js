8066b802533103b71be90190887125a67088e2cd
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-03 23:30:40 +0530

    adding age variables codesto file

--- keywords/variables.js
+++ keywords/variables.js
@@ -3,8 +3,8 @@
 // var variablename = value; 
 /*
 *here use var keyword before assigning any variable and then type the varable name you want to assign and then put assignment operator, i.e, the equals to sign and then put the value to want to put inside the variable.
-
-var name = yuvi;
+*/
+var name = "yuvi";
 var age = 20;
 // now we assigned the name and the age into the variable 
 age = age + 1;
