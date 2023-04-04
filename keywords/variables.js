43a5be2a9dca72a6abb3cb272fbbdabe0a93f53b
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-04 22:35:30 +0530

    adding new codes to my file

--- keywords/variables.js
+++ keywords/variables.js
@@ -4,15 +4,21 @@
 /*
 *here use var keyword before assigning any variable and then type the varable name you want to assign and then put assignment operator, i.e, the equals to sign and then put the value to want to put inside the variable.
 */
+
 var name = "yuvi";
 var age = 20;
+
 // now we assigned the name and the age into the variable 
+
 age = age + 1;
+
 /**
  * but when yuvi become 21cyears old then we changed our stored variable by adding 1 to the stored data in age 
  */
+
 var greetings = "Hello" ;
 greetings + "world !" ;
 
 var totalAfterTax = 53.03; // uses camelCase if the variable name is multiple words
+
 var tip = 8; // uses lowercase if the variable name is one word
