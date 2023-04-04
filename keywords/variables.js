5221b308ad567b0f1cd291d843c6295c5cc79a50
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-04 22:32:27 +0530

    adding new codes to my file

--- keywords/variables.js
+++ keywords/variables.js
@@ -13,3 +13,6 @@ age = age + 1;
  */
 var greetings = "Hello" ;
 greetings + "world !" ;
+
+var totalAfterTax = 53.03; // uses camelCase if the variable name is multiple words
+var tip = 8; // uses lowercase if the variable name is one word
