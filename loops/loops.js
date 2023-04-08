e91aae078f369adc452b6fb5545bbefddf00ec1d
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-08 11:41:56 +0530

    updating my loop file

--- loops/loops.js
+++ loops/loops.js
@@ -1,4 +1,12 @@
-for(var i = 0; i < 10; i++)
-{
-    console.log(i);
+// for(var i = 0; i < 10; i++)
+// {
+//     console.log(i);
+// }
+
+
+var my_string = "Udacity";
+
+// Iterate using a Loop
+for (var i = 0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt(i));
 }
