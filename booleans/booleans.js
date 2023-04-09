1fdd6529878d0f1c96168d0c7e2b933e479975ce
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-09 09:39:08 +0530

    adding comments in code

--- booleans/booleans.js
+++ booleans/booleans.js
@@ -6,4 +6,4 @@ if (haveEnrolledInCourse){
 	console.log("Welcome "+studentName+" to Udacity!"); // Will run only if haveEnrolledInCourse is true
 }
 else
-	console.log("go to hell")
+	console.log("go to hell")// now the output will shoe else part because the statment is false
