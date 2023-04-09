005693008ee203babac47bc306368769c2a2d73a
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-09 09:37:02 +0530

    adding codes in booleans

--- booleans/booleans.js
+++ booleans/booleans.js
@@ -1,7 +1,9 @@
-var studentName = "John";
+var studentName = "Yuvi";
 var haveEnrolledInCourse = true;
 var haveCompletedTheCourse = false;
 
 if (haveEnrolledInCourse){ 
 	console.log("Welcome "+studentName+" to Udacity!"); // Will run only if haveEnrolledInCourse is true
 }
+else
+	console.log("go to hell")
