160cad57a2d0bfc7247c41c1ac1fe98ab1f28194
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-09 09:27:45 +0530

    adding txt about boolens to my file

---
+++ booleans/booleans.js
@@ -0,0 +1,7 @@
+var studentName = "John";
+var haveEnrolledInCourse = true;
+var haveCompletedTheCourse = false;
+
+if (haveEnrolledInCourse){ 
+	console.log("Welcome "+studentName+" to Udacity!"); // Will run only if haveEnrolledInCourse is true
+}
