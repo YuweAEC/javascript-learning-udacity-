650b5daccd8ae9c39cce8d28d21079c4cbbe3728
Yuvraj Singh <yuwe18b@gmail.com>
2023-04-14 22:10:21 +0530

    updating the files to new folder

---
+++ data-types/booleans/strings/stings.js
@@ -0,0 +1,17 @@
+// testing string concatenation
+
+console.log(" Hello! "+" Yuvraj Singh ")
+
+/**
+ * practising the indexing in the javascript
+ */
+console.log("james"[1])
+
+var name=["james"];
+name[0];
+
+/**
+ * practising the newline and gap code characters in strings
+ */
+
+console.log("Yuvraj is \"bolu boi\" \t \n \t and \n \t we all know he can be clever in the time of need")
