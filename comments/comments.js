f5020c8dfc994b06ff3c8581f3826a28444562a6
Yuvraj Singh <117657410+YuweAEC@users.noreply.github.com>
2023-06-09 21:41:34 +0530

    Update comments.js

--- comments/comments.js
+++ comments/comments.js
@@ -1,8 +1,10 @@
 // function reverses the order of characters in a word
+
 function rev(w)
 {
     var revw = "";
     // loops over characters in reverses order
+
     for (var i=w.length - 1; i >= 0; i--)
     {
         // adds characters to a new word
