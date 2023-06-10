2ea0816aa225dd9b0537dccd43f7f927224e881b
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-10 23:37:33 +0530

    add lines of comments for scope to code

--- pracfile.js
+++ pracfile.js
@@ -11,4 +11,9 @@ accountCity = "Bihar"
 
 console.log(accountId);
 
+/**
+ * prefer not to use var 
+ * because of the issue in block scope and functional scope
+ */
+
 console.table([accountEmail,accountId,accountCity,accountPassword])
