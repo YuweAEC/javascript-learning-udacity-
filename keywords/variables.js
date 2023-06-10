5ac402c9bc3eece74eff67103ec2e1ff1546984b
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-11 00:04:18 +0530

    remove lines form files code

--- keywords/variables.js
+++ keywords/variables.js
@@ -22,8 +22,6 @@ greetings + "world !" ;
 var totalAfterTax = 53.03; // uses camelCase if the variable name is multiple words
 var tip = 8; // uses lowercase if the variable name is one word
 
-
-
 /**
  * finding fahrenheit and celcius using variables
  */
