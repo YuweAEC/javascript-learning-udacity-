fc77a074667d89a870dd83a4e341782e3e9b97d2
Yuvraj Singh <yuwe18b@gmail.com>
2024-01-05 20:17:57 +0530

    refactor: update some changes

--- keywords/variables.js
+++ keywords/variables.js
@@ -22,7 +22,7 @@ greetings + "world !" ;
 var totalAfterTax = 53.03; // uses camelCase if the variable name is multiple words
 var tip = 8; // uses lowercase if the variable name is one word
 
-/**
+/*
  * finding fahrenheit and celcius using variables
  */
 
